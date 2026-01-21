e42023a4c809f0151282631f1bed9b19ccf8369f
Umer Farooq <farooq.umer1602@gmail.com>
2026-01-21 21:33:49 +0500

    REs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
   const {cart} = useCart();
   return (
     <div className='sticky  top-2 z-30'>
-      <div className='grid grid-cols-3 lg:bg-red-700 md:bg-amber-600 sm:bg-green-600 sm:w-full bg-white rounded-2xl w-[80%] m-auto shadow-2xl  h-15 px-10'>
+      <div className='grid grid-cols-[20%_35%_45%]  lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 lg:bg-red-700  md:bg-amber-600 sm:bg-green-600  bg-gray-700  rounded-2xl md:w-[90%]  lg:w-[80%] px-2 m-auto shadow-2xl md:px-5 sm:px-3 h-15 lg:px-10'>
           <div className=''>
            <NavLink to={"/"}> <img  className='w-25 h-11' src={logo} alt="" /></NavLink>
           </div>
-          <div className='flex justify-center  items-center '>
+          <div className='flex lg:justify-center md:justify-center sm:justify-center justify-end  items-center '>
             <ul className='flex text-[12px] gap-3.5 uppercase'>
               <NavLink to={"/Mens"} className={({isActive})=>
               isActive && "border-b-red-600 border-b"} ><li className="hover:font-semibold hover:scale-105 transition-all duration-300">Men</li></NavLink>
@@ -24,10 +24,10 @@ const Navbar = () => {
             </ul>
           </div>
           <div className='flex items-center justify-end '>
-            <ul className='flex gap-3.5'>
+            <ul className='flex lg:gap-3.5 md:gap-3.5 sm:gap-3.5 text-[13px] gap-x-2'>
               <NavLink to={"/"} className={({isActive})=>
               isActive && "border-b-red-600 border-b"}><li className="hover:font-semibold hover:scale-105 transition-all duration-300">Home</li></NavLink>
-              <li>About</li>
+              <li className=''>About</li>
                <Link to={"/Cart"}> <li className='relative'><LocalMallIcon/>
                {
                 cart.length > 0 && (
